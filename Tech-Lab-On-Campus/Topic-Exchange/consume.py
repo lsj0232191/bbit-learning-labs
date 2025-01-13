2dad762e07c8db040b28a7b6f202820c2dba33e3
Hajin Jang <hajinjang25@gmail.com>
2025-01-13 15:45:29 -0500

    consumer part

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -14,6 +14,7 @@
 
 import argparse
 import sys
+import pika
 
 from solution.consumer_sol import mqConsumer  # pylint: disable=import-error
 
@@ -23,10 +24,9 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    
+    bindingKey = '#.' + sector + '.#'
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
-    
 
 
 if __name__ == "__main__":
@@ -35,5 +35,7 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    sector = sys.argv[1]
+    queueName = sys.argv[2]
 
-    sys.exit(main(sector,queue))
+    sys.exit(main(sector,queueName))
--- Tech-Lab-On-Campus/Topic-Exchange/solution/consumer_sol.py
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/consumer_sol.py
@@ -21,7 +21,9 @@ class mqConsumer(mqConsumerInterface):
         # Create Queue if not already present
         self.channel.queue_declare(queue=self.queue_name)
         # Create the exchange if not already present
-        self.exchange = self.channel.exchange_declare(self.exchange_name)
+        self.exchange = self.channel.exchange_declare(
+            exchange=self.exchange_name, exchange_type="topic"
+        )
         # Bind Binding Key to Queue on the exchange
         self.channel.queue_bind(
             queue=self.queue_name,
